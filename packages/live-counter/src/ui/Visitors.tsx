64447849f810637d5c25f323f51b81b406b45b11
muadpn <muadpn434@gmail.com>
2025-02-05 20:42:52 +0530

    test: testing tailwind config

--- packages/live-counter/src/ui/Visitors.tsx
+++ packages/live-counter/src/ui/Visitors.tsx
@@ -2,5 +2,14 @@ import { useVisitorTracker } from "@/provider/live-tracker";
 
 export default function Visitors() {
   const { visitorCount } = useVisitorTracker();
-  return <div>{visitorCount}</div>;
+  return (
+    <div>
+      <div className="text-teal-500">blue</div>
+      <div className="bg-black">
+        <p className="text-white">black</p>
+      </div>
+      <div className="bg-green-400">Green</div>
+      <p>{visitorCount}</p>
+    </div>
+  );
 }
